d49625a3f3e19d460094927646039a6ecb904e8e
Brendon Almeida <brendon.sssoares@gmail.com>
2024-09-25 22:09:36 -0300

    alteração3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
 
 function Home() {
+return(
+<div className='header'>salve</div>
+)
+
 }
+export default Home;
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,14 +3,18 @@ import React, { useState } from 'react';
 function Login() {
   return( 
   <div className="container">
+    <div className="header">
+      <h1>Condelivery</h1>
+      </div>
     <div className="login-form">
-    <h1>Condelivery</h1>
-      <h2>Login</h2>
-      <input type="text" placeholder="Digite seu e-mail ou código de usuário" />
-      <input type="password" placeholder="Digite sua senha" />
-      <button>Entrar</button>
+      <h2 className="login">Login</h2>
+      <input className= "text-box" type="text" placeholder="Digite seu e-mail" />
+      <input className= "text-box" type="password" placeholder="Digite sua senha" />
+      <button >Entrar</button>
       <p>Não possui uma conta? <a href="#">Cadastre-se</a></p>
+      <p>Ao Cadastrar-se, você concorda com nossos <a href="#">Termos</a>, <a href="#">Política de Privacidade</a> e <a href="#">Política de Cookies</a>.</p>
     </div>
+      <div className='footer'>-</div>
   </div>)
 }
 export default Login;
