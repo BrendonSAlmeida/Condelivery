a38898a9c9acbd750818afcf905a29b668364311
Brendon Almeida <brendon.sssoares@gmail.com>
2024-09-26 22:10:11 -0300

    alteração5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './Home.css';
 // import Login from './components/Login';
 import AppRoutes from './components/Routes';
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
 
-
 function Home() {
+    const [active, setActive] = useState(false);
+
+    const toggleMode = () => {
+      setActive(!active);
+    };
+  
     return (
-        <div className="App">
-          <div>
-            <div className="hamburger hamburgerIcon"></div>
-          </div>
-          <div className="list">
-            <ul className="listItems">
-              <li>HOME</li>
-              <li>PRODUCTS</li>
-              <li>CONTACT</li>
-              <li>ABOUT US</li>
+      <div className="hamburguerAll">
+        <div className=''>fef4efg
+        <div className={active ? "icon iconActive" : "icon"} onClick={toggleMode}>
+        <div className="hamburger hamburgerIcon"></div>
+        </div>
+        <div className={active ? "menu menuOpen" : "menu menuClose"}>
+            <div className='list'>
+             <ul className="listItems">
+                <li>Home</li>
+                <li>Configurações</li>
+                <li>Perfil</li>
+                <li>Sair</li>
             </ul>
           </div>
         </div>
-      );
-
-}
+        </div> 
+      </div>
+      
+    );
+  }
+  
 export default Home;
