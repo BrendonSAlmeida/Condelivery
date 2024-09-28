818aac10bb78acf16a545578cd339341f583edc3
Brendon Almeida <brendon.sssoares@gmail.com>
2024-09-28 19:54:47 -0300

    alteração8

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
-import AppRoutes from './Routes';
 import { useNavigate } from "react-router-dom";
+
 
 
 function Login() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');   
+  const [errorMessage,] = useState('');
 
-  const handleLogin   
- = () => {
-    //adicionar a lógica de autenticação
-    // Se a autenticação for bem-sucedida, direcionar para Home
-    navigate('/home');
+  const handleLogin = () => {
+    console.log("Email:", email);
+    console.log("Senha:", password);
+   
+    if (email === 'teste@outlook.com' && password === '123fiap') {
+      navigate('/Home');} 
+    else {
+      alert('Email ou senha inválidos.');
+    }
   };
   return( 
   <div className='all'>
@@ -20,14 +27,27 @@ function Login() {
       </div>
     <div className="login-form">
       <h2 className="login">Login</h2>
-      <input className= "text-box" type="text" placeholder="Digite seu e-mail" />
-      <input className= "text-box" type="password" placeholder="Digite sua senha" />
+      <input
+        className="text-box"
+        type="text"
+        placeholder="Digite seu e-mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="text-box"
+        type="password"
+        placeholder="Digite sua senha"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button onClick={handleLogin}>Entrar</button>
-      <p>Não possui uma conta? <a href="#">Cadastre-se</a></p>
-      <p className='cad'>Ao Cadastrar-se, você concorda com nossos <a href="#">Termos</a>, <a href="#">Política de Privacidade</a> e <a href="#">Política de Cookies</a>.</p>
-      </div>
+    <p>Não possui uma conta? <a href="/cadastro">Cadastre-se</a></p>
+    <p className='cad'>Ao Cadastrar-se, você concorda com nossos <a href="/termos">Termos</a>, <a href="/privacidade">Política de Privacidade</a> e <a href="/cookies">Política de Cookies</a>.</p>
+</div>
     </div>
-    <div className='footer'>-</div>
+    {errorMessage && <p>{errorMessage}</p>}
+    <div className='footer'></div>
   </div>)
 }
 export default Login;
